0975fe9dc94ffa13e567615bd3da231a4e2141e4
zulfiqarlaili <zulfiqarlaili@gmail.com>
2023-11-04 01:30:19 +0800

    chore: refactor code

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import { ANON_KEY, SUPABASE_PROJECT_URL } from './constants'
 
-const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4cXN6ZnJxbXNudXp1ZXR5amd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1MTY5NDcsImV4cCI6MjAxNDA5Mjk0N30._YKbyH4J1p7gbVW_tEOW-CnoJATL6COPeNZi9hrfi7s'
-const projectUrl = 'https://gxqszfrqmsnuzuetyjgz.supabase.co'
-export const supabase = createClient(projectUrl, anonKey)
+export const supabase = createClient(SUPABASE_PROJECT_URL, ANON_KEY)
