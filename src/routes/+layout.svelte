0aa2736aacd5267ef74986bc187a6bb708eaf060
zulfiqarlaili <zulfiqarlaili@gmail.com>
2024-01-03 11:59:09 +0800

    feat: add typebot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,42 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { onMount } from 'svelte';
+
 	inject({ mode: dev ? 'development' : 'production' });
 
-	import Feedback from '$lib/Feedback.svelte';
 	let showFeedbackDialog = false;
 
 	function handleButton() {
 		showFeedbackDialog = true;
 	}
+	onMount(() => {
+		const typebotInitScript = document.createElement('script');
+		typebotInitScript.type = 'module';
+		typebotInitScript.innerHTML = 
+		`import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.25/dist/web.js'
+
+		Typebot.initBubble({
+  		typebot: "tak-sedap",
+  		apiHost: "https://bot.07102020.xyz",
+  		theme: {
+    		placement: "left",
+    		button: { backgroundColor: "#5380e9" },
+    		previewMessage: { backgroundColor: "#c7c7c7" },
+  		},
+		});
+		`;
+		document.body.append(typebotInitScript);
+	});
 </script>
 
 <div class="nav-bar">
 	<a href="/"><h1>Tak Sedap</h1></a>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
-	<i on:click={handleButton} class="scale-element feedback fa-regular fa-comment" />
+	<!-- <i on:click={handleButton} class="scale-element feedback fa-regular fa-comment" /> -->
 </div>
-<Feedback bind:isOpen={showFeedbackDialog} />
+<!-- <Feedback bind:isOpen={showFeedbackDialog} /> -->
 <small>beta-release</small>
 <slot />
 
